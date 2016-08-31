938071b8263737b843c337e9738cf900080b485c
glory-ych <gloryforme@163.com>
2016-08-31 14:22:08 +0800

    f

--- provider/src/main/java/com/ytud/provider/impl/ProjectApiImpl.java
+++ provider/src/main/java/com/ytud/provider/impl/ProjectApiImpl.java
@@ -15,12 +15,12 @@ public class ProjectApiImpl implements ProjectApi {
     @Resource
     private ProjectService projectService;
 
-    
+
     public ProjectModel getProject(int id) {
         System.out.println("start...");
         Project project = projectService.getProject(id);
 
         System.out.println("end...");
-return new ProjectModel(project.getId(), project.getProjectCode(), project.getProjectName());
+        return new ProjectModel(project.getId(), project.getProjectCode(), project.getProjectName());
     }
 }
